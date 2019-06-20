d252f87f8435bf7c531d95252f40a57af09e861f
Khushdeep Sidhu <khushdeep009@gmail.com>
2019-06-20 00:06:53 -0400

    Created delete HTTP endpoints and seperate route files

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const path = require ( 'path' )
 const express = require ( 'express' )
 require ( './db/mongoose' )
-const User = require ( './models/user' )
-const Task = require ( './models/task' )
+const userRouter = require ( './routers/user' )
+const taskRouter = require ( './routers/task' )
 
 const app = express ()
 
@@ -18,153 +18,11 @@ app.use ( express.static ( publicDirPath ) )
 // Parse incoming JSON into a JavaScript object which you can access on req.body. 
 app.use ( express.json () )
 
-// HTTP end point to create user
-app.post ( '/users', async ( req, res ) => {
+// Register userRouter
+app.use ( userRouter )
 
-    const user = new User ( req.body )
-
-    try {
-        await user.save ()
-        res.status ( 201 ).send ( savedUser )
-    } catch ( error ) {
-        res.status ( 400 ).send ( error )
-    }
-
-} )
-
-// HTTP end point to read multiple users
-app.get ( '/users', async ( req, res ) => {
-
-    try {
-
-        const users = await User.find ( {} )
-        if ( !users.length ) {
-            return res.sendStatus ( 404 )
-        }
-    
-        res.send ( users )
-    } catch ( error ) {
-        res.status ( 500 ).send ( error )
-    } 
-    
-} )
-
-// HTTP end point to read a single user
-app.get ( '/users/:id', async ( req, res ) => {
-
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById ( _id )
-        if ( !user ) {
-            return res.sendStatus ( 404 )
-        }
-        res.status ( 200 ).send ( user )
-
-    } catch ( error ) {
-        res.status ( 500 ).send ( error )
-    }
-
-} )
-
-// HTTP end point to update a user 
-app.patch ( '/users/:id', async ( req, res ) => {
-
-    const updates = Object.keys ( req.body )
-    console.log ( updates )
-    const allowedUpdates = [ 'name', 'age', 'email', 'password' ]
-    const isValidOperation = updates.every ( ( update ) => allowedUpdates.includes ( update ) )
-
-    if ( !isValidOperation ) {
-        return res.status( 400 ).send ( 'Invalid Updates' )
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate ( req.params.id, req.body, { new: true, runValidators: true } )
-        if ( !user ) {
-            return res.sendStatus ( 404 )
-        }
-        res.send ( user )
-    } catch ( error ) {
-        res.status ( 400 ).send ( error )
-    }
-
-} )
-
-// HTTP end point to create a task
-app.post ( '/tasks', async ( req, res ) => {
-
-    const task = new Task ( req.body )
-
-    try {
-        await task.save ()
-        res.status( 201 ).send ( task )
-    } catch ( error ) {
-        res.status( 400 ).send ( error )
-    }
-
-} )
-
-// HTTP end point to read multiple tasks
-app.get ( '/tasks', async ( req, res ) => {
-
-    try {
-
-        const tasks = await Task.find ( {} )
-        if ( !tasks.length ) {
-            return res.sendStatus ( 404 )
-        }
-        res.send ( tasks )
-
-    } catch ( error ) {
-        res.status ( 500 ).send ( error )
-    }
-
-} )
-
-// HTTP end point to read a task
-app.get ( '/tasks/:id', async ( req, res ) => {
-
-    const _id = req.params.id
-
-    try {
-        const task = await Task.findById ( _id )
-        if ( !task ) {
-            return res.sendStatus ( 404 )
-        }
-        res.send ( task )
-
-    } catch ( error ) {
-        res.status ( 500 ).send ( error )
-    }
-
-} )
-
-// HTTP end point to update a task
-app.patch ( '/tasks/:id', async ( req, res ) => {
-
-    const updates = Object.keys ( req.body )
-    const allowedUpdates = [ 'description', 'completed' ]
-    const isValidOperation = updates.every ( ( update ) => allowedUpdates.includes ( update ) )
-
-    if ( !isValidOperation ) {
-        return res.status ( 400 ).send ( 'Invalid Updates' )
-    }
-
-    try {
-
-        const task = await Task.findByIdAndUpdate ( req.params.id, req.body, { new: true, runValidators: true } )
-        if ( !task ) {
-            return res.sendStatus ( 404 )
-        }
-
-        res.send ( task )
-
-    } catch ( error ) {
-        res.status ( 400 ).send ( error )
-    }
-
-} )
+// Register task router
+app.use ( taskRouter )
 
 app.listen ( port, () => {
     console.log ( `Server is up running at port ${port}` )
---
+++ src/routers/user.js
@@ -0,0 +1,95 @@
+const express = require ( 'express' )
+const User = require ( '../models/user' )
+
+const router = new express.Router ()
+
+// HTTP end point to create user
+router.post ( '/users', async ( req, res ) => {
+
+    const user = new User ( req.body )
+
+    try {
+        const savedUser = await user.save ()
+        res.status ( 201 ).send ( savedUser )
+    } catch ( error ) {
+        res.status ( 400 ).send ( error )
+    }
+
+} )
+
+// HTTP end point to read multiple users
+router.get ( '/users', async ( req, res ) => {
+
+    try {
+
+        const users = await User.find ( {} )
+        if ( !users.length ) {
+            return res.sendStatus ( 404 )
+        }
+    
+        res.send ( users )
+    } catch ( error ) {
+        res.status ( 500 ).send ( error )
+    } 
+    
+} )
+
+// HTTP end point to read a single user
+router.get ( '/users/:id', async ( req, res ) => {
+
+    const _id = req.params.id
+
+    try {
+        const user = await User.findById ( _id )
+        if ( !user ) {
+            return res.sendStatus ( 404 )
+        }
+        res.status ( 200 ).send ( user )
+
+    } catch ( error ) {
+        res.status ( 500 ).send ( error )
+    }
+
+} )
+
+// HTTP end point to update a user 
+router.patch ( '/users/:id', async ( req, res ) => {
+
+    const updates = Object.keys ( req.body )
+    console.log ( updates )
+    const allowedUpdates = [ 'name', 'age', 'email', 'password' ]
+    const isValidOperation = updates.every ( ( update ) => allowedUpdates.includes ( update ) )
+
+    if ( !isValidOperation ) {
+        return res.status( 400 ).send ( 'Invalid Updates' )
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate ( req.params.id, req.body, { new: true, runValidators: true } )
+        if ( !user ) {
+            return res.sendStatus ( 404 )
+        }
+        res.send ( user )
+    } catch ( error ) {
+        res.status ( 400 ).send ( error )
+    }
+
+} )
+
+// HTTP end point to delete a user
+router.delete ( '/users/:id', async ( req, res ) => {
+
+    try {
+        const user = await User.findByIdAndDelete ( req.params.id )
+        if ( !user ) {
+            return res.sendStatus ( 404 )
+        }
+
+        res.send ( user )
+    } catch ( error ) {
+        res.sendStatus ( 500 )
+    }
+
+} )
+
+module.exports = router
